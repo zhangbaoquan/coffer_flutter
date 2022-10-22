fa4c4b67dc0f069f30682c8d4fab9ecaab990512
coffer <zbq19930108@126.com>
2022-10-22 17:17:55 +0800

    code message : 组件UI练习

--- lib/sample/main_page.dart
+++ lib/sample/main_page.dart
@@ -7,9 +7,7 @@ import 'package:coffer_flutter/sample/res_page.dart';
 import 'package:coffer_flutter/sample/stateful_page.dart';
 import 'package:coffer_flutter/sample/stateless_page.dart';
 import 'package:coffer_flutter/sample/topNavigator_page.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'animate_page2.dart';
 import 'animate_page3.dart';
 import 'bottomNavigator_page.dart';
@@ -44,7 +42,7 @@ class _MyAppState extends State<MyApp> {
         ),
         home: Scaffold(
           appBar: AppBar(
-            title: Text('导航使用'),
+            title: const Text('导航使用'),
           ),
           body: _MainItem(),
         ),
@@ -87,13 +85,15 @@ class _MyAppState extends State<MyApp> {
                 useNightTheme = value;
               });
             }),
-        RouterNavigator(),
+        const RouterNavigator(),
       ],
     );
   }
 }
 
 class RouterNavigator extends StatefulWidget {
+  const RouterNavigator({super.key});
+
   @override
   State<RouterNavigator> createState() => _RouterNavigatorState();
 }
@@ -104,97 +104,126 @@ class _RouterNavigatorState extends State<RouterNavigator> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          SwitchListTile(
-              title: Text('${byName ? '' : '不'}通过路由名跳转'),
-              value: byName,
-              onChanged: (value) {
-                setState(() {
-                  byName = value;
-                });
-              }),
-          Wrap(
-            spacing: 10,
-            children: [
-              _item('插件使用', PluginUse(), 'plugin'),
-              _item('资源使用', ResPage(), 'res'),
-            ],
-          ),
-          Wrap(
-            spacing: 10,
-            children: [
-              _item('StatefulUsePage', StatefulUsePage(), 'stateful'),
-              _item('StatelessUsePage', StatelessUse(), 'stateless'),
-            ],
-          ),
-          Wrap(
-            direction: Axis.horizontal,
-            spacing: 10,
-            alignment: WrapAlignment.center,
-            children: [
-              _item('layout', FlutterLayoutPage(), 'layout'),
-              _item('手势处理', GesturePage(), 'gesture'),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10,
-                ),
-                child: _item('组件生命周期', WidgetLifecyclePage(), 'lifecycle'),
+    return Column(
+      children: [
+        SwitchListTile(
+            title: Text('${byName ? '' : '不'}通过路由名跳转'),
+            value: byName,
+            onChanged: (value) {
+              setState(() {
+                byName = value;
+              });
+            }),
+        Wrap(
+          spacing: 10,
+          children: [
+            _item('插件使用', PluginUse(), 'plugin'),
+            _item('资源使用', ResPage(), 'res'),
+          ],
+        ),
+        Wrap(
+          spacing: 10,
+          children: [
+            _item('StatefulUsePage', StatefulUsePage(), 'stateful'),
+            _item('StatelessUsePage', StatelessUse(), 'stateless'),
+          ],
+        ),
+        Wrap(
+          direction: Axis.horizontal,
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            _item('layout', FlutterLayoutPage(), 'layout'),
+            _item('手势处理', GesturePage(), 'gesture'),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
               ),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 10,
-                ),
-                child: _item('应用生命周期', AppLifecyclePage(), 'AppLifecycle'),
+              child: _item('组件生命周期', WidgetLifecyclePage(), 'lifecycle'),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10,
               ),
-            ],
-          ),
-          Wrap(
-            direction: Axis.horizontal,
-            alignment: WrapAlignment.center,
-            spacing: 10,
-            children: [
-              _item('动画', AnimatePage(), 'animate'),
-              _item('动画2', AnimateWidgetPage(), 'animate2'),
-              _item('动画3', AnimateBuilderPage(), 'animate3'),
-              _item('Hero 动画1', HeroAnimationPage(), 'Hero1'),
-              _item('Hero 动画1', HeroAnimationPage(), 'Hero1'),
-            ],
-          ),
-          Wrap(
-            spacing: 10,
-            children: [
-              _item('顶部导航', TabbedAppBarPage(), 'TopTab'),
-              _item('底部导航', BottomTabNavigatorPage(), 'BottomTab'),
-              _item('侧拉导航', DrawerPage(), 'DrawerPage'),
-            ],
-          ),
-          _item('哈哈', LalaPage(), 'lala'),
-          // _item('开启三方应用', LaunchPage(),'launch'),
-        ],
-      ),
+              child: _item('应用生命周期', AppLifecyclePage(), 'AppLifecycle'),
+            ),
+          ],
+        ),
+        Wrap(
+          direction: Axis.horizontal,
+          alignment: WrapAlignment.center,
+          spacing: 10,
+          children: [
+            _item('动画', AnimatePage(), 'animate'),
+            _item('动画2', AnimateWidgetPage(), 'animate2'),
+            _item('动画3', AnimateBuilderPage(), 'animate3'),
+            _item('Hero 动画1', HeroAnimationPage(), 'Hero1'),
+          ],
+        ),
+        Wrap(
+          spacing: 10,
+          children: [
+            _item('顶部导航', TabbedAppBarPage(), 'TopTab'),
+            _item('底部导航', BottomTabNavigatorPage(), 'BottomTab'),
+            _item('侧拉导航', DrawerPage(), 'DrawerPage'),
+          ],
+        ),
+        _item2('哈哈', LalaPage(), 'lala'),
+      ],
     );
   }
 
   _item(String title, page, String routeName) {
-    return Container(
-      child: ElevatedButton(
-        onPressed: () {
-          // true 表示用路由名跳转，false 表示使用Navigator跳转
-          if (byName) {
-            Navigator.pushNamed(context, routeName);
-          } else {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => page));
-          }
-        },
-        child: Text(title),
+    return ElevatedButton(
+      onPressed: () {
+        // true 表示用路由名跳转，false 表示使用Navigator跳转
+        if (byName) {
+          Navigator.pushNamed(context, routeName);
+        } else {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => page));
+        }
+      },
+      child: Text(title),
+    );
+  }
+
+  _item2(String title, page, String routeName) {
+    return SizedBox(
+      width: 80,
+      height: 50,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+            gradient:
+                LinearGradient(colors: [Colors.red, Colors.orange.shade700]),
+            // 渐变
+            borderRadius: BorderRadius.circular(3),
+            boxShadow: const [
+              // 阴影
+              BoxShadow(
+                  color: Colors.black54,
+                  offset: Offset(2.0, 2.0),
+                  blurRadius: 4.0)
+            ]),
+        child: Center(
+          child: GestureDetector(
+            onTap: () {
+              // true 表示用路由名跳转，false 表示使用Navigator跳转
+              if (byName) {
+                Navigator.pushNamed(context, routeName);
+              } else {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => page));
+              }
+            },
+            child: Text(title),
+          ),
+        ),
       ),
     );
   }
